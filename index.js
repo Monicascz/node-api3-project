b3847b6a9376ee9b1ac47bfa0767bace671829a3
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2021-04-08 14:07:00 -0700

    chanes in idex.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // require your server and launch it
 const server = require('./api/server.js')
 const env = require("dotenv").config()
-const express = require("express")
-const app = express()
+// const express = require("express")
+// const app = express()
 const port = process.env.PORT || 9000
 
-app.use("/api/", (_,res)=>{
+server.use("/api/", (_,res)=>{
     res.json({data: "API is connected! :) "})
 })
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log(`Listening on port ${port}`)
 })
 
