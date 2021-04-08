aaf1a81b7c179c490846ef6d7907dd20cfea646f
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2021-04-08 14:01:21 -0700

    added env to connect to API

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 // require your server and launch it
 const server = require('./api/server.js')
+const env = require("dotenv").config()
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 9000
 
-server.listen(1234, ()=>{
-    console.log('server running on 1234')
+app.use("/api/", (_,res)=>{
+    res.json({data: "API is connected! :) "})
 })
+
+app.listen(port, ()=>{
+    console.log(`Listening on port ${port}`)
+})
+
+
+// server.listen(1234, ()=>{
+//     console.log('server running on 1234')
+// })
