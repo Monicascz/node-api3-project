963ae4d64bd0304dea930e4f5bdf502f9c3e35b7
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2021-04-08 14:58:03 -0700

    work

--- api/server.js
+++ api/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const morgan = require('morgan'); // if you don't put in a path then it just reads this as a node module.
+//const morgan = require('morgan'); // if you don't put in a path then it just reads this as a node module.
 //morgan is a testing tool that gives us some information about our request (displays in the terminal)
 //tells us what type of request it was, endpoint was, what the status was and what it returns, and how long it took (how fast the server is)
-const helmet = require("helmet") // protects the headers from knowing what information is being used.
+//const helmet = require("helmet") // protects the headers from knowing what information is being used.
 
 const mw = require('./middleware/middleware.js')
 const usersRouter = require('./users/users-router.js')
@@ -14,8 +14,8 @@ const server = express();
 
 
 server.use(express.json()) // every endpoint uses this global middleware
-server.use(morgan("dev")) // to use morgan you have to initialize the library and pass in "dev" to let morgan know that we are in a development environment.
-server.use(helmet()) // you need to initialize helmet. 
+//server.use(morgan("dev")) // to use morgan you have to initialize the library and pass in "dev" to let morgan know that we are in a development environment.
+//server.use(helmet()) // you need to initialize helmet. 
 
 server.use('/api/users',  usersRouter)
 server.use(mw.logger) // every endpoint uses this global middleware
